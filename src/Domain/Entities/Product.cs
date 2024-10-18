5f715ced09ddb247ddcc9c5a5d26e3224293be3f
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2024-10-18 14:05:29 +0600

    feat: refactor

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -6,12 +6,9 @@ namespace AspNetCore.WebApi.Template.Domain.Entities;
 public class Product : AuditableEntity
 {
     public string Name { get; set; }
-    public int? SupplierId { get; set; }
     public int? CategoryId { get; set; }
-    public string QuantityPerUnit { get; set; }
-    public decimal? UnitPrice { get; set; }
-    public short? UnitsInStock { get; set; }
-    public short? UnitsOnOrder { get; set; }
+    public string Unit { get; set; } = "Pcs"; // Pcs, Kg, ...
+    public decimal? Price { get; set; }
     public short? ReorderLevel { get; set; }
     public bool Discontinued { get; set; }
 
